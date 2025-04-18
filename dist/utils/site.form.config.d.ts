f4c20c995298f9d4486729da052af970be9acf75
Assanee Saksiritantawan <149568680+assanee-zenith@users.noreply.github.com>
2025-04-18 10:34:40 +0700

    * chore(site.form.config): add SITES_FORM_ID_PRODUCT_PURCHASE_SUPPLIER to site form config * chore(package.json): update version to 0.0.21

--- dist/utils/site.form.config.d.ts
+++ dist/utils/site.form.config.d.ts
@@ -51,6 +51,7 @@ interface SiteFormConfigType {
     SITES_FORM_ID_PRODUCT_TRANSFERS: string;
     SITES_FORM_ID_PRODUCT_VARIANTS: string;
     SITES_FORM_ID_ONLINE_STORE_BLOG: string;
+    SITES_FORM_ID_PRODUCT_PURCHASE_SUPPLIER: string;
 }
 declare const SiteFormConfig: SiteFormConfigType;
 export default SiteFormConfig;
